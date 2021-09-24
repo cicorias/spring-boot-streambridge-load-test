c4b67c6c8afbe2f70760eecb3415c4d54a4a81c3
Shawn Cicoria <github@cicoria.com>
2021-09-24 13:49:42 -0400

    rabbit mq working

--- src/main/java/stream/load/web/IngestController.java
+++ src/main/java/stream/load/web/IngestController.java
@@ -1,15 +1,17 @@
 package stream.load.web;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.cloud.stream.function.StreamBridge;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageHandlingException;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,29 +25,38 @@ public class IngestController {
   Logger logger = LoggerFactory.getLogger(IngestController.class);
 
   private final ObjectMapper mapper;
+  private final StreamBridge streamBridge;
 
-    @PostMapping
-    public ResponseEntity<ResponseAcknowledgement> sendMessage(@RequestBody String payload) throws MessageHandlingException {
+  @PostMapping
+  public ResponseEntity<ResponseAcknowledgement> sendMessage(@RequestBody String payload)
+      throws MessageHandlingException {
 
-      try {
-        JsonNode jsonPayload = mapper.readTree(payload);
-        String binding = jsonPayload.at("/data/direction").asText("nowhere");
-        int ackValue = jsonPayload.at("/ack").asInt(-1);
-        String correlationId = jsonPayload.at("/correlationId").asText("no-correlation");
-        logger.info("using binding={} for correlationId={}", binding, correlationId);
+    try {
+      JsonNode jsonPayload = mapper.readTree(payload);
+      String binding = jsonPayload.at("/data/direction").asText("nowhere");
+      int ackValue = jsonPayload.at("/ack").asInt(-1);
+      String correlationId = jsonPayload.at("/correlationId").asText("no-correlation");
+      logger.info("using binding={} for correlationId={}", binding, correlationId);
 
-        if (ackValue != -1) {
-          ResponseAcknowledgement acknowledgement = new ResponseAcknowledgement();
-          acknowledgement.setEvtSeq(ackValue);
-          return ResponseEntity.ok(acknowledgement);
-        } else {
-          return ResponseEntity.noContent().build();
-        }
+      sendMessage(binding, jsonPayload, correlationId);
 
-      } catch (JsonProcessingException e) {
-        e.printStackTrace();
-        return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+      if (ackValue != -1) {
+        ResponseAcknowledgement acknowledgement = new ResponseAcknowledgement();
+        acknowledgement.setEvtSeq(ackValue);
+        return ResponseEntity.ok(acknowledgement);
+      } else {
+        return ResponseEntity.noContent().build();
       }
+
+    } catch (JsonProcessingException e) {
+      e.printStackTrace();
+      return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
     }
-    
+  }
+
+  void sendMessage(String bindingName, JsonNode data, String partitionKey) {
+    Message<?> message = MessageBuilder.withPayload(data).build();// TODO: future but not in repro.setHeader("partitionKey", partitionKey).build();
+    streamBridge.send(bindingName, message);
+  }
+
 }
